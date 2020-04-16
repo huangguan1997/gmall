3377c6d1b72f9573ae22afa33ae6b2a21905aaf6
huangguan <1875596816@qq.com>
2020-04-16 18:44:38 +0800

    项目创建并构建完成

---
+++ gmall-prms/src/main/java/com/atguigu/gmall/prms/GmallPrmsApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.prms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallPrmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallPrmsApplication.class, args);
+    }
+
+}
---
+++ gmall-prms/src/test/java/com/atguigu/gmall/prms/GmallPrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.prms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallPrmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
